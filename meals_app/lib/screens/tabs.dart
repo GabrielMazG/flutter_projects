f5365d97c95fa3e9de4bef92e69bd57c79a7a89f
Gabriel Maz <g.maz@globant.com>
2024-12-16 16:25:27 +0100

    MealsApp Add bottom menu navigation

---
+++ meals_app/lib/screens/tabs.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TabsStateState();
+  }
+}
+
+class _TabsStateState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    var activeTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: []);
+      activeTitle = 'Your Favorites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activeTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
